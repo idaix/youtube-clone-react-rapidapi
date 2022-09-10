bde8601eea196a52f992aac8adb68efb617305e2
daishek <daichekkal@gmail.com>
2022-09-10 15:21:40 +0100

    search feed page, search functionality

--- src/pages/SearchFeed.jsx
+++ src/pages/SearchFeed.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Videos } from '../components'
+import { fetchFromApi } from '../utils/fetchFromApi'
 const SearchFeed = () => {
+
   const { q } = useParams()
+
+  const [videos, setVideos] = useState([])
+
+  useEffect(()=>{
+    fetchFromApi(`search?part=snippet&q=${q}`)
+    .then(data => {
+      setVideos(data.items)
+    })
+  }, [q])
+  
   return (
-    <div>Search for {q}</div>
+      <section className='p-2'>
+          <h1 className=" text-primary font-bold mb-2 ">{q.toUpperCase()}</h1>
+          <Videos videos={videos} />
+      </section>
   )
+
 }
 
 export default SearchFeed
